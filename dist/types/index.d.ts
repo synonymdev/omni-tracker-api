9f0a2f4f7eeadd26bd3dcb28b415d9d4c3ceb6da
Corey Phillips <corey.lyle.phillips@protonmail.com>
2022-02-16 09:34:29 -0500

    Re-built dist directory.

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,10 +1,12 @@
+import { Result } from './utils/result';
+import { IOmniDecodeTransactionResponse, ListReceivedByAddressData, TGetOmniBalanceResponse, TGetUserStateResponse, TOmniGetAllBalancesForAddressResponse } from './types';
 export default class OmniTrackerApi {
     private readonly url;
     constructor(url?: string);
-    omniGetAllBalancesForAddress(address: any): Promise<import("./utils/result").Ok<import("./types").TOmniGetAllBalancesForAddressResponse> | import("./utils/result").Err<unknown>>;
-    getOmniBalance(address: string, propertyId: string | number): Promise<import("./utils/result").Ok<number> | import("./utils/result").Err<unknown>>;
-    listReceivedByAddress(address: string): Promise<import("./utils/result").Ok<import("./types").ListReceivedByAddressData[]> | import("./utils/result").Err<unknown>>;
-    getBalanceByAddress(address: string): Promise<import("./utils/result").Ok<number> | import("./utils/result").Err<unknown>>;
-    getUserState(userId: string, nodeId: string): Promise<import("./utils/result").Ok<import("./types").TGetUserStateResponse> | import("./utils/result").Err<unknown>>;
-    omniDecodeTransaction(txHex: string): Promise<import("./utils/result").Ok<import("./types").IOmniDecodeTransactionResponse> | import("./utils/result").Err<unknown>>;
+    omniGetAllBalancesForAddress(address: any): Promise<Result<TOmniGetAllBalancesForAddressResponse>>;
+    getOmniBalance(address: string, propertyId: string | number): Promise<Result<TGetOmniBalanceResponse>>;
+    listReceivedByAddress(address: string): Promise<Result<ListReceivedByAddressData[]>>;
+    getBalanceByAddress(address: string): Promise<Result<number>>;
+    getUserState(userId: string, nodeId: string): Promise<Result<TGetUserStateResponse>>;
+    omniDecodeTransaction(txHex: string): Promise<Result<IOmniDecodeTransactionResponse>>;
 }
